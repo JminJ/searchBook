cd272564af7ae422736b2ed6bd0a3d228f388c0c
JminJ <jminju254@gmail.com>
2020-09-19 23:26:55 +0900

    get item's count

--- find_book.py
+++ find_book.py
@@ -1,21 +1,39 @@
+from urllib.request import urlopen
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import time
+import re
+import math
 
-url = 'https://book.naver.com/' #driver를 통해 접속할 사이트 주소
+bookName = '인간문제'
 
-def search_book(bookName):
+url = 'https://book.naver.com/search/search.nhn?sm=sta_hty.book&sug=&where=nexearch&query={bookName}' #driver를 통해 접속할 사이트 주소
+
+def search_book(url):
     driver = webdriver.Chrome('D:\Minju_project\chromedriver.exe') #웹 드라이버의 위치
     driver.get(url)
 
-    assert "네이버 책" in driver.title
-    elem = driver.find_element_by_id("nx_query") #네이버 책 사이트의 검색 창의 html id
-    elem.send_keys(bookName)    
+    try:
+        element = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'txt_block')))
+        book_list = []
+        pageNum = re.findall('\d+',driver.find_element_by_xpath("//div[@id='content']/div[@class = 'tit_area']/span[@class='num num2']/strong").text)
+        print(pageNum)
+        pageNum = final_pageNum(int(pageNum[0]))
+        print(type(pageNum))
+    except TimeoutException:    # 예외 처리
+        print('해당 페이지에 책 정보가 존재하지 않습니다.')
     
-    elem = driver.find_element_by_id("search_btn") #네이버 책 사이트의 검색 버튼의 html id
-    elem.click()
+    print(pageNum)
+   
+def final_pageNum(pageNum):
+    return math.ceil(pageNum / 20) # 받아온 총 책 정보를 한 페이지에 보여지는 양으로 니눠 페이지 수를 계산
 
-    time.sleep(10)
-    driver.qiut()
 
-search_book("데미안")
+search_book(url)
+
+
+
 
